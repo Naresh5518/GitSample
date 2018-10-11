c9193f5f1bd7f914f6b86bb97c382fe2a5c10224
mTL NARESH <mtlnaresh@mtl-naresh.local>
2018-10-11 14:52:42 +0530

    term

--- GitSample/ViewController.swift
+++ GitSample/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("hai naresh")
         print("hello world")
+        print("hgshaS")
     }
 
 
