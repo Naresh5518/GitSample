b983263b0de156c6dc0704bc44265a25f233c869
mTL NARESH <mtlnaresh@mtl-naresh.local>
2018-10-15 11:31:42 +0530

    Initial Commit

---
+++ GitHubExample/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  GitHubExample
+//
+//  Created by mTL NARESH on 15/10/18.
+//  Copyright © 2018 mTL NARESH. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+-(void)printSumOfNumbers{
+    int a=109,b=120;
+    NSLog(@"Sum Of Numbers Is %d",a+b);
+}
+
+@end
